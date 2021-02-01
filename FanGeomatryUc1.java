fc1669a09bf33c719157dc816472d5e06f588804
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 20:26:32 +0530

    welcome to the line comparision computation program

---
+++ FanGeomatryUc1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FanGeomatryUc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("welcome to the line comparision computation program");
+         }
+       }
